aaa8d46f1c3ad2ca04a1b04ee303a9451cb937a9
Vibhor Gupta <vibhor.88.gupta@gmail.com>
2021-10-18 05:42:32 +0000

    # Use of nextline Characters

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ print("Hello World")
 # WAP To show the output as provided!
 
 print('what to print')
+
+# Use of nextline Characters
+
+print("Hi !!\n How \tare \byou !!How\f is\r everything\t going\v? ")
+
+# 
